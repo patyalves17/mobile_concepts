c7cd118944c039fa6bf9f61e530ff2c522126362
Paty Alves <patriciaalves10@gmail.com>
2020-06-24 10:24:16 -0300

    text

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function App() {
-  return <View />;
+  return (
+    <View>
+      <Text>Hi</Text>
+      <Text>Hi</Text>
+      <Text>Hi</Text>
+      <Text>Hi</Text>
+      <Text>Hi</Text>
+    </View>
+  );
 }
