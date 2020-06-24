d6ffe1cfbb7ce236446629e7385f601c096eb847
Paty Alves <patriciaalves10@gmail.com>
2020-06-24 18:39:56 -0300

    getting projects from api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,48 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  SafeAreaView,
+  FlatList,
+  Text,
+  StyleSheet,
+  StatusBar,
+} from 'react-native';
+
+import api from './service/api';
 
 export default function App() {
-  return <View />;
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <StatusBar barStyle='light-content' backgroundColor='#7159c1' />
+
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({ item }) => (
+            <Text style={styles.projects}>{item.title}</Text>
+          )}
+        ></FlatList>
+      </SafeAreaView>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+  },
+
+  projects: {
+    color: '#fff',
+    fontSize: 20,
+  },
+});
